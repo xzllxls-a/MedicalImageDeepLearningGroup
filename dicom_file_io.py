247c2ebef9049dca4508bf55367749cd7efcda32
MedicalImageDeepLearningGroup <xzllxlsai@gmail.com>
2026-03-05 20:43:55 +0800

    DICOM file IO python script and DICOM data image file.

--- dicom_file_io.py
+++ dicom_file_io.py
@@ -5,15 +5,28 @@ Created on 2026 Feb 27
 '''
 
 import pydicom
-
-
-
+import cv2
+import numpy as np
 
 # File path.
-file_path = ""
-
+file_path = r"data\samples\CT\I200.dcm"
 
 if __name__ == "__main__":
-    # Read the specified DICOM files.
+    print("DICOM image file input and output.")
+    
+    # Read the specified DICOM files as a dataset data structure.
     ds = pydicom.read_file(file_path)
-    ds.
+    
+    # Print the dataset of DICOM file header.
+    print(ds)
+    
+    # Convert the DICOM dataset to numpy data format.
+    image = ds.pixel_array
+    print("max = " + str(np.max(image)))
+    print("min = " + str(np.min(image)))
+    # image = (image.astype(np.int32) - 32768).astype(np.int16)  # Calibrate the bias of the gray image.
+    
+    cv2.imshow("CT", image)
+    
+    key = cv2.waitKey(0)  # 等待按键命令, 1000ms 后自动关闭
+    # destroyAllWindows()
